01c03e833698e991a97a6ba68b6aeacb0ac71971
Shpakouski <job.shpakouski@gmail.com>
2020-11-10 15:59:55 +0300

    styling

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
 let duration;
-const timer = new Timer(durationInput, startButton, pauseButton,{
+const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart(totalDuration) {
-       duration = totalDuration;
+        duration = totalDuration;
     },
     onTick(timeRemaining) {
-        circle.setAttribute('stroke-dashoffset', 
+        circle.setAttribute('stroke-dashoffset',
             (perimeter * timeRemaining) / duration - perimeter
         );
     },
--- timer.js
+++ timer.js
@@ -1,9 +1,9 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton, callbacks){
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
-        if(callbacks) {
+        if (callbacks) {
             this.onStart = callbacks.onStart;
             this.onTick = callbacks.onTick;
             this.onComplete = callbacks.onComplete;
@@ -14,29 +14,29 @@ class Timer {
     }
 
     start = () => {
-        if(this.onStart) {
+        if (this.onStart) {
             this.onStart(this.timeRemaining);
         }
         this.tick();
-        this.intervalId = setInterval(this.tick,20);
+        this.intervalId = setInterval(this.tick, 20);
     }
-    
+
     pause = () => {
         clearInterval(this.intervalId);
     }
 
     tick = () => {
-        if(this.timeRemaining <= 0){
+        if (this.timeRemaining <= 0) {
             this.pause();
-            if(this.onComplete) {
+            if (this.onComplete) {
                 this.onComplete();
             }
         } else {
             this.timeRemaining = this.timeRemaining - 0.02;
-            if(this.onTick) {
+            if (this.onTick) {
                 this.onTick(this.timeRemaining);
             }
-        } 
+        }
     }
 
     get timeRemaining() {
